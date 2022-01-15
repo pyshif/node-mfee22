88f9b4e2c9d0206c863341b405c5f5954e4baf1a
Po Yu Chen <pyshif@gmail.com>
2022-01-15 11:57:37 +0800

    add catch error to crawler3.js

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,23 +2,23 @@ const fs = require('fs/promises');
 const axios = require('axios');
 
 (async function () {
-  // step 1: 讀檔 stock.txt
-  const stockNo = await fs.readFile('./stock.txt', 'utf-8');
-  // console.log(stockNo);
+    // step 1: 讀檔 stock.txt
+    const stockNo = await fs.readFile('./stock.txt', 'utf-8');
+    // console.log(stockNo);
 
-  // step 2: axios 發送請求
-  const domain = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY'; 
-  const response = await axios.get(domain, {
-    params: {
-      response: 'json',
-      date: '20220101',
-      stockNo
+    // step 2: axios 發送請求
+    try {
+        const domain = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY';
+        const response = await axios.get(domain, {
+            params: {
+                response: 'json',
+                date: '20220101',
+                stockNo,
+            },
+        });
+
+        console.log(response.data);
+    } catch (err) {
+        console.error(err);
     }
-  });
-
-  console.log(response.data);
-}());
-
-
-
-
+})();
